a6c278d757fa2d82670ed730a0b1fe23d19b433d
TIZI <tenidiotzinternetudefault@gmail.com>
2024-10-23 17:21:58 +0200

    Initial commit

---
+++ TCPConnection.hpp
@@ -0,0 +1,17 @@
+//
+// Created by TIZI on 05/10/2024.
+//
+
+#ifndef BC_TCPCONNECTION_HPP
+#define BC_TCPCONNECTION_HPP
+
+
+
+class TCPConnection {
+public:
+    TCPConnection();
+private:
+};
+
+
+#endif //BC_TCPCONNECTION_HPP
