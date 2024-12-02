a1238e33feff1a000d36cf2f347b3acc702fcaa3
TIZI <tenidiotzinternetudefault@gmail.com>
2024-12-02 15:04:46 +0100

    Implemented TcpClient constructor

--- core/TCPConnection.cpp
+++ core/TCPConnection.cpp
@@ -6,7 +6,14 @@
 #include "boost/asio.hpp"
 #include "TCPConnection.hpp"
 
+
 TCPConnection::TCPConnection() {
-    boost::asio::io_context ioc;
     std::cout << "i live";
+}
+
+TcpClient::TcpClient(boost::asio::io_context iocontext, const std::string& ipString, int port) :
+        _socket(iocontext) {
+    ip::address ip = ip::address::from_string(ipString);
+    auto endpoint = ip::tcp::endpoint(ip, port);
+    _socket.connect(endpoint);
 }
--- core/TCPConnection.hpp
+++ core/TCPConnection.hpp
@@ -5,12 +5,29 @@
 #ifndef BC_TCPCONNECTION_HPP
 #define BC_TCPCONNECTION_HPP
 
-
+using namespace boost::asio;
 
 class TCPConnection {
 public:
     TCPConnection();
 private:
+};
+
+
+class TcpServer {
+public:
+    TcpServer(boost::asio::io_context ioContext);
+private:
+    ip::tcp::socket _socket;
+
+};
+
+class TcpClient {
+public:
+    TcpClient(boost::asio::io_context iocontext, const std::string& ipString, int port);
+private:
+    boost::asio::io_context _ioc;
+    ip::tcp::socket _socket;
 };
 
 
