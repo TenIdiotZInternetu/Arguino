a20094e7d4269100e11f0fcc251557ec9b4a3e3a
touster <tenidiotzinternetudefault@gmail.com>
2025-11-16 16:59:07 +0100

    Removed deprecated code

--- gui/Main/MainController.cs
+++ gui/Main/MainController.cs
@@ -21,35 +21,12 @@ public static class MainController {
         await tcpClient.ConnectAsync();
         Console.WriteLine("Connected!");
         Adapter = tcpClient.Handler;
-        // Adapter.StateChangedEvent += TestRead;
         
         AppInitializedEvent?.Invoke();
 
         while (true) {
             await tcpClient.SendMessageAsync("R");
             await Task.Delay(10);
-        }
-    }
-
-    private static void TestRead(ArduinoState state) {
-        Console.WriteLine($"Pin 3: {state.GetDigital(3)}");
-    }
-    
-    private static void TestClient() {
-        var client = new TestTcpClient(8888);
-        
-        while (!client.Connect()) {
-            Console.WriteLine("Waiting for connection");
-            Thread.Sleep(3000);
-        }
-        
-        Console.WriteLine("Connected!");
-
-        while (true) {
-            client.WriteMessage("R>::<");
-            string message = client.ReadMessage(">::<");
-            Console.WriteLine(message);
-            Thread.Sleep(1000);
         }
     }
 }
