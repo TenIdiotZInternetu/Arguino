5706ec61342c89fe2127807b0c18ea0c6a20ab6b
touster <tenidiotzinternetudefault@gmail.com>
2026-01-21 19:48:53 +0100

    Passing current through LED

--- gui/ComponentManagement/Components/Led/Led.cs
+++ gui/ComponentManagement/Components/Led/Led.cs
@@ -11,13 +11,20 @@ public class Led : Component {
 
     public override void OnRisingEdge(Pin pin) {
         UpdateSprite(SPRITE_LED_ON);
+        PassCurrent(pin);
     }
 
     public override void OnFallingEdge(Pin pin) {
         UpdateSprite(SPRITE_LED_OFF);
+        PassCurrent(pin);
     }
 
-    private void PassCurrent(Pin pin) {
+    private void PassCurrent(Pin inputPin) {
+        Pin outputPin = inputPin.Name == PIN_ANODE ?
+            GetPin(PIN_CATHODE) :
+            GetPin(PIN_ANODE);
         
+        outputPin.SetCurrent(inputPin.Current);
+        outputPin.SetVoltage(inputPin.Voltage);
     }
 }
