a75658e4041dd36ed6dd107a4e64bdf3674c7fe9
touster <tenidiotzinternetudefault@gmail.com>
2026-01-31 19:00:15 +0100

    Implementing holding down control on Alt press

--- gui/Main/Views/TemplatedControls/ComponentControl.cs
+++ gui/Main/Views/TemplatedControls/ComponentControl.cs
@@ -12,6 +12,7 @@ namespace Gui.Views.TemplatedControls;
 
 public class ComponentControl : Control {
      private Component _component = null!;
+     private bool _pressHeld = false;
 
      public static readonly DirectProperty<ComponentControl, Component> ComponentProperty = AvaloniaProperty.RegisterDirect<ComponentControl, Component>(
           nameof(Component), o => o.Component, (o, v) => o.Component = v);
@@ -50,15 +51,45 @@ public class ComponentControl : Control {
           bool leftPressed = e.Properties.IsLeftButtonPressed;
           var position = e.GetCurrentPoint(this).Position;
           
-          if (leftPressed) {
+          if (leftPressed && !_pressHeld) {
                _component.OnControlPress(position.ToVector2());
           }
+          
+          _pressHeld = e.KeyModifiers.HasFlag(KeyModifiers.Alt);
      }
 
      protected override void OnPointerReleased(PointerReleasedEventArgs e) {
           var position = e.GetCurrentPoint(this).Position;
-          _component.OnControlRelease(position.ToVector2());
+
+          if (!_pressHeld) {
+               _component.OnControlRelease(position.ToVector2());
+          }
      }
      
+     protected override void OnAttachedToVisualTree(VisualTreeAttachmentEventArgs e) {
+          base.OnAttachedToVisualTree(e);
+
+          var topLevel = TopLevel.GetTopLevel(this);
+          if (topLevel != null) {
+               topLevel.KeyUp += OnGlobalKeyUp;
+          }
+     }
+
+     protected override void OnDetachedFromVisualTree(VisualTreeAttachmentEventArgs e) {
+          base.OnDetachedFromVisualTree(e);
+
+          var topLevel = TopLevel.GetTopLevel(this);
+          if (topLevel != null) {
+               topLevel.KeyUp -= OnGlobalKeyUp;
+          }
+     }
+
+     private void OnGlobalKeyUp(object? _, KeyEventArgs e) {
+          if (_pressHeld && e.PhysicalKey == PhysicalKey.AltLeft) {
+               _component.OnControlRelease(new Vector2());
+               _pressHeld = false;
+          } 
+     }
+
      private void InvalidateVisual(SKSvg _)  => InvalidateVisual();
 }
