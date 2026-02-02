656c3525ae1c40c8e5d72b608792c996b25a9a8c
TenIdiotZInternetu <TenIdiotZInternetu@gmail.com>
2026-02-02 11:54:20 +0100

    _isInitialized = true;

--- gui/Main/MainController.cs
+++ gui/Main/MainController.cs
@@ -46,6 +46,7 @@ public static class MainController {
         }
         
         GlobalTimer = Stopwatch.StartNew();
+        _isInitialized = true;
         AppInitializedEvent?.Invoke();
     }
 
