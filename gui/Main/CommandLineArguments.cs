bd75dbedfc897a52f668968abd06daa6a9ff63dd
TenIdiotZInternetu <TenIdiotZInternetu@gmail.com>
2026-02-02 11:24:01 +0100

    Created CommandLineArguments

---
+++ gui/Main/CommandLineArguments.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CommandLine;
+
+namespace Gui;
+
+public class CommandLineArguments {
+    [Option('s', "scene", Default = "./scene.yaml", HelpText = "Path to the .yaml scene defintion")]
+    public string ScenePath { get; set; } = "./scene.yaml";
+
+    [Option('c', "components", Default = "./ComponentManagement/Components", HelpText = "Path to the directory of components definitions")]
+    public string ComponentsPath { get; set; } = "./ComponentManagement/Components";
+
+    [Option('p', "--port", Default = 8888, HelpText = "Port of the core's TCP server")]
+    public int TcpPort { get; set; }
+
+    [Option("no-tcp", Default = false, HelpText = "Turn off connection to TCP server at initialization")]
+    public bool NoTcp { get; set; }
+}
+        
