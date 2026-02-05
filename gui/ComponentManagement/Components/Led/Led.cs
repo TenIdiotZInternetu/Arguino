aa814da61f03ad44296df501d9227f7c9bc0e5b0
TenIdiotZInternetu <TenIdiotZInternetu@gmail.com>
2026-02-05 12:47:47 +0100

    Digitized LED

--- gui/ComponentManagement/Components/Led/Led.cs
+++ gui/ComponentManagement/Components/Led/Led.cs
@@ -9,31 +9,20 @@ public class Led : Component {
     private const string SPRITE_LED_ON = "ledOn";
     private const string SPRITE_LED_OFF = "ledOff";
 
-    private const string PIN_ANODE = "anode";
     private const string PIN_CATHODE = "cathode";
+    private const string PIN_ANODE = "anode";
 
-    private Pin _anode;
     private Pin _cathode;
+    private Pin _anode;
 
     public Led(string definitionPath) : base(definitionPath) {
         UpdateSprite(SPRITE_LED_OFF);
-        _anode = GetPin(PIN_ANODE);
         _cathode = GetPin(PIN_CATHODE);
+        _cathode.MakeReadOnly();
+        _anode = GetPin(PIN_ANODE);
     }
 
-    public override void OnInputChange(Pin pin, float voltage) {
-        float anodeVoltage = _anode.IsConnected ?
-            _anode.Voltage :
-            Pin.GND_VOLTAGE;
-
-        bool conducts = _cathode.Voltage - anodeVoltage > FORWARD_VOLTAGE;
-
-        if (conducts) {
-            _anode.SetVoltage(voltage);
-            UpdateSprite(SPRITE_LED_ON);
-        }
-        else {
-            UpdateSprite(SPRITE_LED_OFF);
-        }
+    public override void OnPinStateChanged(Pin pin) {
+        UpdateSprite(_cathode.IsHigh ? SPRITE_LED_ON : SPRITE_LED_OFF);
     }
 }
