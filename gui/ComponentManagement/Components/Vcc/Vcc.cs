8d2168953dd135c52c1f9b46e985038e2a062c5e
TenIdiotZInternetu <TenIdiotZInternetu@gmail.com>
2026-02-05 12:50:13 +0100

    Digitized VCC

--- gui/ComponentManagement/Components/Vcc/Vcc.cs
+++ gui/ComponentManagement/Components/Vcc/Vcc.cs
@@ -11,17 +11,12 @@ public class Vcc : Component {
 
     // TODO: Make voltage adjustable parameter in config/in main window
     public const float VOLTS = 5;
-    private const uint PIN = 0;
+
+    private Pin _pin;
 
     public Vcc(string definitionPath) : base(definitionPath) {
-        GetPin(PIN).MakeWriteOnly();
-    }
-
-    public override void OnPinConnected(Pin pin) {
-        pin.SetVoltage(VOLTS);
-    }
-
-    public override void OnPinDisconnected(Pin pin) {
-        pin.SetVoltage(Pin.GND_VOLTAGE);
+        _pin = GetPin(0);
+        _pin.MakeWriteOnly();
+        _pin.SetHigh();
     }
 }
