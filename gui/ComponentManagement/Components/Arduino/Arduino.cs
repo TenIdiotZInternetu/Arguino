141401548d00bede9059265d4e3f1b1028239cfd
TenIdiotZInternetu <TenIdiotZInternetu@gmail.com>
2026-02-05 16:54:45 +0100

    fun with svgs

---
+++ gui/ComponentManagement/Components/Arduino/Arduino.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ComponentManagement.Components;
+
+public class Arduino : Component
+{
+    public Arduino(string definitionPath) : base(definitionPath) {}
+
+    
+}
