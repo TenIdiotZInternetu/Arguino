630c76409427fbc0aa35a3590da5f52edaecaaed
touster <tenidiotzinternetudefault@gmail.com>
2026-02-26 23:34:46 +0100

    More renames

--- core/shared-memory-adapter/IpcHandler.cpp
+++ core/shared-memory-adapter/IpcHandler.cpp
@@ -1,32 +1,32 @@
-#include "ShmemHandler.hpp"
+#include "IpcHandler.hpp"
 
-#include "ShmemRegion.hpp"
+#include "MemoryRegion.hpp"
 
 namespace ipc = boost::interprocess;
 
 namespace arguino::shmem {
 
-ShmemHandler::ShmemHandler(const std::string& shmemName, size_t sizeInPages)
+IpcHandler::IpcHandler(const std::string& shmemName, size_t sizeInPages)
     : _name(shmemName),                                               //
       _pages(sizeInPages),                                            //
       _shmemObject(ipc::create_only, _name.c_str(), ipc::read_write)  //
 {
     size_t shmem_size = _pages * ipc::mapped_region::get_page_size();
     _shmemObject.truncate(shmem_size);
-    _writer = std::make_unique<ShmemRegion>(_shmemObject, 0, size() / 2);
+    _producer = std::make_unique<MemoryRegion>(_shmemObject, 0, size());
 }
 
-ShmemHandler::~ShmemHandler()
+IpcHandler::~IpcHandler()
 {
     ipc::shared_memory_object::remove(_name.c_str());
 }
 
-void ShmemHandler::write(size_t offset, uint8_t byte)
+void IpcHandler::write(size_t offset, uint8_t byte)
 {
-    *(uint8_t*)_writer->at(offset) = byte;
+    *(uint8_t*)_producer->at(offset) = byte;
 }
 
-size_t ShmemHandler::size()
+size_t IpcHandler::size()
 {
     ipc::offset_t size;
     _shmemObject.get_size(size);
