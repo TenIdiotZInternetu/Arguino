11398d50044368e4dffb3fb339aa9058b76cfa40
touster <tenidiotzinternetudefault@gmail.com>
2026-02-26 22:08:42 +0100

    Renamed ShmemWriter to ShmemRegion

--- core/shared-memory-adapter/ShmemRegion.cpp
+++ core/shared-memory-adapter/ShmemRegion.cpp
@@ -1,10 +1,10 @@
-#include <ShmemWriter.hpp>
+#include <ShmemRegion.hpp>
 
 namespace ipc = boost::interprocess;
 
 namespace arguino::shmem {
 
-ShmemWriter::ShmemWriter(const shmem_t& shmemObject, size_t offset, size_t size)
+ShmemRegion::ShmemRegion(const shmem_t& shmemObject, size_t offset, size_t size)
     : _mappedRegion(shmemObject, ipc::read_write, offset, size)
 {}
 
