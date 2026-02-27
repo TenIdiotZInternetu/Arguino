587198b1ad732879a01ef38b1aeafebd8802dda8
touster <tenidiotzinternetudefault@gmail.com>
2026-02-27 16:53:23 +0100

    Rewritten CircularBuffers wrtie implementation with offsets

--- core/shared-memory-adapter/MemoryRegion.hpp
+++ core/shared-memory-adapter/MemoryRegion.hpp
@@ -15,15 +15,24 @@ class MemoryRegion {
     MemoryRegion(const shmem_t& shmemObject, size_t offset, size_t size);
 
     size_t size() { return _mappedRegion.get_size(); }
-    void* begin() { return _mappedRegion.get_address(); }
-    void* end() { return (uint8_t*)begin() + size(); }
+    uint8_t* begin() { return (uint8_t*)_mappedRegion.get_address(); }
+    uint8_t* end() { return begin() + size(); }
 
-    void* at(size_t offset) { return (uint8_t*)begin() + offset; }
-    size_t offset(void* ptr) { return (uint8_t*)ptr - (uint8_t*)begin(); }
+    template <typename T>
+    T& at(size_t offset);
+    uint8_t* at(size_t offset) { return begin() + offset; }
+
+    size_t offset(void* ptr) { return (uint8_t*)ptr - begin(); }
 
    private:
     shmem_region_t _mappedRegion;
 };
+
+template <typename T>
+inline T& MemoryRegion::at(size_t offset)
+{
+    return *reinterpret_cast<T*>(at(offset));
+}
 
 }  // namespace arguino::shmem
 
