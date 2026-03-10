7f1976baca5120ce73dae7f6e019457408e22b44
TenIdiotZInternetu <TenIdiotZInternetudefault@gmail.com>
2026-03-10 11:50:00 +0100

    Appliying component transformations to canvas

--- gui/Main/Utils/ExtensionMethods/GeometryExtensions.cs
+++ gui/Main/Utils/ExtensionMethods/GeometryExtensions.cs
@@ -24,10 +24,21 @@ public static class GeometryExtensions {
 
     public static SKMatrix ToSKMatrix(this ComponentManagement.Scenes.Transform transform) {
         // TODO: Translate to coordinates origin, then translate
+        var originShift = SKMatrix.CreateTranslation(-transform.BaseSize.X / 2, -transform.BaseSize.Y / 2);
+        var inverseShift = SKMatrix.CreateTranslation(transform.BaseSize.X / 2, transform.BaseSize.Y / 2);
+
         var rotation = SKMatrix.CreateRotationDegrees(transform.Rotation);
         var scale = SKMatrix.CreateScale(transform.ScaleX, transform.ScaleY);
         var translation = SKMatrix.CreateTranslation(transform.PositionX, transform.PositionY);
 
-        return SKMatrix.Concat(translation, SKMatrix.Concat(rotation, scale));
+        return originShift
+            .MulitplyLeft(rotation)
+            .MulitplyLeft(scale)
+            .MulitplyLeft(translation)
+            .MulitplyLeft(inverseShift);
+    }
+
+    public static SKMatrix MulitplyLeft(this SKMatrix self, SKMatrix other) {
+        return SKMatrix.Concat(other, self);
     }
 }
