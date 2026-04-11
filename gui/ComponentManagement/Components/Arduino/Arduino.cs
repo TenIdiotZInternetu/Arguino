359b89b410c5bb6402bd04a8a6ae97df97d77814
touster <tenidiotzinternetudefault@gmail.com>
2026-04-11 17:39:29 +0200

    Removed ArduinoState from the Frontend

--- gui/ComponentManagement/Components/Arduino/Arduino.cs
+++ gui/ComponentManagement/Components/Arduino/Arduino.cs
@@ -1,15 +1,12 @@
 using ComponentManagement.Graph;
 using TcpAdapter;
 using IpcAdapter;
+using IpcAdapter.Events;
 
 namespace ComponentManagement.Components;
 
 public class Arduino : Component {
-    private ArduinoState _state = new();
-    
-    // TODO: Replace by interface
     private IIpcAdapter? _ipc;
-    
     private readonly SynchronizationContext _uiContext;
 
     public Arduino(string definitionPath) : base(definitionPath) {
@@ -18,8 +15,7 @@ public class Arduino : Component {
 
     public override void OnPinStateChanged(Pin pin) {
         uint idx = GetPinIndex(pin.Name!);
-        _state.DigitalPins[idx] = pin.IsHigh;
-
+        
         if (_ipc?.IsConnected ?? false) {
             // TODO: _ipc.SendEvent();
         }
@@ -31,23 +27,6 @@ public class Arduino : Component {
         _ipc.ReceivedEventEvent += e => {
             // TODO: _uiContext.Post(_ => , null);    
         };
-    }
-
-    private void UpdateCircuit(ArduinoState newState) {
-        for (int i = 0; i < ArduinoState.DIGITAL_PIN_COUNT; i++) {
-            Pin pin = GetDigitalPin(i);
-            bool pinValue = newState.DigitalPins[i];
-            var pinMode = newState.PinModes[i];
-
-            if (pinMode == ArduinoState.PinMode.In) {
-                pin.MakeReadOnly();
-            }
-
-            if (pinMode == ArduinoState.PinMode.Out) {
-                pin.MakeWriteOnly();
-                pin.SetValue(pinValue);
-            }
-        }
     }
 
     private Pin GetDigitalPin(int index) => GetPin("D" + index)!;
